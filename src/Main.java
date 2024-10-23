769590fc93b9b9424b594e213ca0b1dac2b73f6a
AidaTegeran <aidategeran@gmail.com>
2024-10-22 21:41:21 -0500

    twoSum

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int [] nums = {5,8,3,4};
+        int [] indices = twoSum(nums, 9);
+
+    }
+
+
+
+}
